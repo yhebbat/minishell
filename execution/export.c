b84f232178406e997681b64029be9b614b818f05
Yassine Hebbat <yhebbat@e2r10p3.1337.ma>
2021-11-14 05:57:52 +0100

    prob in export

--- execution/export.c
+++ execution/export.c
@@ -1,27 +1,83 @@
 #include    "execution.h"
-/*
+
 char    *check_eq(char *str, int *eq)
 {
     int i;
-    char    
+    int k;
+    char *ret;
 
     i = 0;
+    k = 0;
     while (str[i])
     {
-        if (str[i] == '=')
+        if (str[i] == '+' && str[i + 1] != '=')
         {
-
+            *eq = -1;
+            printf("export: `%s': not a valid identifier\n", str);
+            break;
+        }
+        else if (str[i] == '+' && str[i + 1] == '=')
+        {
+            *eq = 2;
+            break;
+        }
+        else if (str[i] == '=')
+        {
+            *eq = 1;
+            break;
         }
         i++;
+        *eq = 0;
     }
-    return (0);
+    if (*eq == 0 || *eq == -1)
+        ret = ft_strdup(str);
+    else if (*eq == 2 || *eq == 1)
+    {
+        ret = malloc(sizeof(char) * (i + 1));
+        if (!ret)
+            return (0);
+        while (str[k] && k < i)
+        {
+            ret[k] = str[k];
+            k++;
+        }
+        ret[k] = '\0';
+    }
+    return (ret);
 }
-*/
+
+char    *fill_exportval(char *str, int *eq)
+{
+    int i;
+    int k;
+    char *ret;
+
+    i = 0;
+    k = 0;
+    while (str[i])
+    {
+        if (str[i] == '+' && str[i + 1] != '=')
+            break;
+        else if (str[i] == '+' && str[i + 1] == '=')
+            break;
+        else if (str[i] == '=')
+            break;
+        i++;
+    }
+    if (*eq == 0 || *eq == -1)
+        ret = NULL;
+    else if (*eq == 1 ||*eq == 2 )
+        ret = ft_strdup(&str[i + *eq]);
+    // else if (*eq == 2)
+    //     ret = ft_strdup(str + i + 2);
+    return (ret);
+}
+
 void    export(t_cmds *cmd, t_exec *exec)
 {
     int t;
     int k;
-    int eq;
+    int *eq;
     char **str;
 
     k = 0;
@@ -33,18 +89,25 @@ void    export(t_cmds *cmd, t_exec *exec)
     }
     while (cmd->args[t])
     {
-        eq = 0;
+        *eq = 0;
+        //eq==0 no =
+        //eq==1 =
+        //eq==2 +=
+        //eq==-1 error not a valid identifier
         k = 0;
-        if (cmd->args[t][k] != '_' && is_alpha(cmd->args[t][k]) == 0)
+        if (cmd->args[t][k] != '_' && ft_isalpha(cmd->args[t][k]) == 0)
         {
             printf("export: `%s': not a valid identifier", cmd->args[t]);
+            break ;
         }
-        while (cmd->args[t][k])
-        {
-            str[0] = check_eq(cmd->args[t], &eq);
-            // str[0] = ft_fill();
-            k++;
-        }
+        // while (cmd->args[t][k])
+        // {
+            str[0] = check_eq(cmd->args[t], eq);
+            str[1] = fill_exportval(cmd->args[t], eq);
+            str[2] = 0;
+            printf("%s||%s||*%d*\n", str[0], str[1],*eq);
+            //k++;
+        //}
         ft_free(str);
         t++;
     }
