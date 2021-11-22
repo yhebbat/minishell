40d3e0daea697f80dd06d0a01fe24c0bbb180d04
yhebbat <yhebat@student.1337.ma>
2021-11-22 20:04:36 +0100

    norm

--- execution/unset.c
+++ execution/unset.c
@@ -1,78 +1,133 @@
 #include    "execution.h"
 
+void	unvalid_unset(t_cmds *cmd, int t)
+{
+	write(2, "export: ", 9);
+	write(2, cmd->args[t], ft_strlen(cmd->args[t]));
+    write(2, ": not a valid identifier\n",26);
+	__get_var(SETEXIT, 1);
+}
+
+void	del1(t_headers *header, t_env *to_del)
+{
+    t_env	*exist_env;
+
+	exist_env = to_del->suivant;
+	header->env_h = exist_env;
+	exist_env->preced = NULL;
+	if (to_del->val != NULL)
+		free(to_del->val);
+	if (to_del->var != NULL)
+		free(to_del->var);
+	free(to_del);
+	to_del = NULL;
+}
+
+void	del2(t_headers *header, t_env *to_del)
+{
+    t_env	*exist_env;
+
+	exist_env = to_del->suivant;
+	exist_env->preced = to_del->preced;
+	exist_env = to_del->preced;
+	exist_env->suivant = to_del->suivant;
+	if (to_del->val != NULL)
+		free(to_del->val);
+	if (to_del->var != NULL)
+		free(to_del->var);
+	free(to_del);
+	to_del = NULL;
+}
+
+void	del3(t_headers *header, t_env *to_del)
+{
+    t_env	*exist_env;
+
+	exist_env = to_del->preced;
+	exist_env->suivant = NULL;
+	header->env_f = exist_env;
+	if (to_del->val != NULL)
+		free(to_del->val);
+	if (to_del->var != NULL)
+		free(to_del->var);
+	free(to_del);
+	to_del = NULL;
+}
+
+void	valid_unset(t_headers *header, t_env *to_del)
+{
+	if (to_del)
+	{
+		if (to_del->preced == NULL && to_del->suivant)
+			del1(header, to_del);
+		else if (to_del->preced && to_del->suivant)
+			del2(header, to_del);
+		else if (to_del->preced && to_del->suivant == NULL)
+			del3(header, to_del);
+	}
+}
+
 void    unset(t_cmds *cmd, t_exec *exec, t_headers *header)
 {
     int t;
-    t_env	*exist_env;
+    // t_env	*exist_env;
     t_env	*to_del;
     t_env	*env;
     char	*str;
 
-    // env = header->env_h;
     t = 1;
     env = header->env_h;
     while (cmd->args[t])
     {
-        //eq==0 no =
-        //eq==1 =
-        //eq==2 +=
-        //eq==-1 error not a valid identifier
-        // k = 0;
         if (cmd->args[t][0] != '_' && ft_isalpha(cmd->args[t][0]) == 0)
-        {
-            printf("export: `%s': not a valid identifier\n", cmd->args[t]);
-            __get_var(SETEXIT,1);
-            // break ;
-        }
+            unvalid_unset(cmd, t);
         else
         {
             str = ft_strdup(cmd->args[t]);
-            // printf("%s||%s||*%d*\n", str[0], str[1],eq);
             to_del = find_var_env(str, env);
-            if (to_del)
-            {
-                // while (env && ft_strcmp(env->var, str) == 0)
-                // {
-                if (to_del->preced == NULL && to_del->suivant)
-                {
-                    exist_env = to_del->suivant;
-                    header->env_h = exist_env;
-                    exist_env->preced = NULL;
-                    if (to_del->val != NULL)
-				        free(to_del->val);
-                    if (to_del->var != NULL)
-                        free(to_del->var);
-                    free(to_del);
-					to_del = NULL;
-                }
-                else if (to_del->preced && to_del->suivant)
-                {
-                    exist_env = to_del->suivant;
-                    exist_env->preced = to_del->preced;
-                    exist_env = to_del->preced;
-                    exist_env->suivant = to_del->suivant;
-                    if (to_del->val != NULL)
-				        free(to_del->val);
-                    if (to_del->var != NULL)
-                        free(to_del->var);
-                    free(to_del);
-					to_del = NULL;
-                }
-                else if (to_del->preced && to_del->suivant == NULL)
-                {
-                   // header->env_f = to_del->preced;
-                    exist_env = to_del->preced;
-                    exist_env->suivant = NULL;
-                    header->env_f = exist_env;
-                    // exist_env->suivant = to_del->suivant;
-                    if (to_del->val != NULL)
-				        free(to_del->val);
-                    if (to_del->var != NULL)
-                        free(to_del->var);
-                    free(to_del);
-					to_del = NULL;
-                }
-    		}
+            valid_unset(header, to_del);
+            // if (to_del)
+            // {
+            //     if (to_del->preced == NULL && to_del->suivant)
+            //     {
+            //         exist_env = to_del->suivant;
+            //         header->env_h = exist_env;
+            //         exist_env->preced = NULL;
+            //         if (to_del->val != NULL)
+			// 	        free(to_del->val);
+            //         if (to_del->var != NULL)
+            //             free(to_del->var);
+            //         free(to_del);
+			// 		to_del = NULL;
+            //     }
+            //     else if (to_del->preced && to_del->suivant)
+            //     {
+            //         exist_env = to_del->suivant;
+            //         exist_env->preced = to_del->preced;
+            //         exist_env = to_del->preced;
+            //         exist_env->suivant = to_del->suivant;
+            //         if (to_del->val != NULL)
+			// 	        free(to_del->val);
+            //         if (to_del->var != NULL)
+            //             free(to_del->var);
+            //         free(to_del);
+			// 		to_del = NULL;
+            //     }
+            //     else if (to_del->preced && to_del->suivant == NULL)
+            //     {
+            //        // header->env_f = to_del->preced;
+            //         exist_env = to_del->preced;
+            //         exist_env->suivant = NULL;
+            //         header->env_f = exist_env;
+            //         // exist_env->suivant = to_del->suivant;
+            //         if (to_del->val != NULL)
+			// 	        free(to_del->val);
+            //         if (to_del->var != NULL)
+            //             free(to_del->var);
+            //         free(to_del);
+			// 		to_del = NULL;
+            //     }
+    		// }
             free(str);
             if (__get_var(GETEXIT,0) != 1)
                 __get_var(SETEXIT,0);
